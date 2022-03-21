dca4e74d74f00d7c37c4fc5d341a80e41ef4c1f6
Zeina <zeinaayman666@gmail.com>
2022-03-21 20:37:16 +0200

    6-9: world

---
+++ src/model/world/Champion.java
@@ -0,0 +1,117 @@
+package model.world;
+//A class representing Champions in the game.
+
+import model.abilities.Ability;
+import model.effects.Effect;
+
+import java.awt.*;
+import java.util.ArrayList;
+
+public class Champion {
+    private String name;
+    private int maxHP;
+    private int currentHP;
+    private int mana;
+    private int maxActionPointsPerTurn;
+    private int currentActionPoints;
+    private int attackRange;
+    private int attackDamage;
+    private int speed;
+    private ArrayList<Ability> abilities;
+    private ArrayList<Effect> appliedEffects;
+    private Condition condition;
+    private Point location;
+
+    public Point getLocation() {
+        return location;
+    }
+
+    public void setLocation(Point location) {
+        this.location = location;
+    }
+
+    public Condition getCondition() {
+        return condition;
+    }
+
+    public void setCondition(Condition condition) {
+        this.condition = condition;
+    }
+
+    public ArrayList<Effect> getAppliedEffects() {
+        return appliedEffects;
+    }
+
+    public ArrayList<Ability> getAbilities() {
+        return abilities;
+    }
+
+    public int getSpeed() {
+        return speed;
+    }
+
+    public void setSpeed(int speed) {
+        this.speed = speed;
+    }
+
+    public int getAttackDamage() {
+        return attackDamage;
+    }
+
+    public void setAttackDamage(int attackDamage) {
+        this.attackDamage = attackDamage;
+    }
+
+    public int getAttackRange() {
+        return attackRange;
+    }
+
+    public int getCurrentActionPoints() {
+        return currentActionPoints;
+    }
+
+    public int getMaxActionPointsPerTurn() {
+        return maxActionPointsPerTurn;
+    }
+
+    public void setMaxActionPointsPerTurn(int maxActionPointsPerTurn) {
+        this.maxActionPointsPerTurn = maxActionPointsPerTurn;
+    }
+
+    public int getMana() {
+        return mana;
+    }
+
+    public int getCurrentHP() {
+        return currentHP;
+    }
+
+    public void setCurrentHP(int currentHP) {
+        this.currentHP = currentHP;
+    }
+
+    public int getMaxHP() {
+        return maxHP;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Champion(String name, int maxHP, int mana, int maxActions, int speed, int attackRange,
+                    int attackDamage){
+        this.name = name;
+        this.maxHP = maxHP;
+        this.mana = mana;
+        this.maxActionPointsPerTurn = maxActions;
+        this.speed = speed;
+        this.attackRange = attackRange;
+        this.attackDamage = attackDamage;
+        condition = Condition.ACTIVE;
+        currentHP = maxHP;
+        currentActionPoints = maxActions;
+        abilities = new ArrayList<>();
+        appliedEffects = new ArrayList<>();
+        location = new Point();
+    }
+}
---
+++ src/model/world/Cover.java
@@ -0,0 +1,27 @@
+package model.world;
+
+import java.awt.*;
+import java.util.Random;
+
+public class Cover {
+    private int currentHP;
+    private Point location;
+
+    public Point getLocation() {
+        return location;
+    }
+
+    public int getCurrentHP() {
+        return currentHP;
+    }
+
+    public void setCurrentHP(int currentHP) {
+        this.currentHP = currentHP;
+    }
+
+    public Cover(int x, int y){
+        Random random = new Random();
+        currentHP = random.nextInt(1000)+100;
+        location = new Point(x,y);
+    }
+}
