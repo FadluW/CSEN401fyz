0b5a14df31105b1bef931183367016282a4ffa54
Zeina <zeinaayman666@gmail.com>
2022-06-07 18:15:35 +0200

    changes in board

--- src/GUI/editingBoard.java
+++ src/GUI/editingBoard.java
@@ -1,6 +1,8 @@
 package GUI;
 
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseMotionListener;
 import java.util.ArrayList;
@@ -20,11 +22,13 @@ import model.world.Cover;
 
 public class editingBoard extends JFrame {
 
-    JLabel champion, player1, player2, info;
+    JLabel champion, player1Label, player2Label, info, ability1Used, ability2Used;
     JTextArea queue, abilitiesInfo, effectsInfo;
-    JPanel abilitiesPanel, effectsPanel, queuePanel;
+    JPanel abilitiesPanel, effectsPanel, queuePanel, endTurnPanel, player1Panel, player2panel, leader1Ability, leader2Ability;
     JLayeredPane panel = new JLayeredPane();
+    JLayeredPane panel2;
     JFrame frame = this;
+    JButton endTurn;
     baseBackground grass = new baseBackground();
     ArrayList<Champion> team1,team2;
     ArrayList<Ability> abilities;
@@ -41,58 +45,37 @@ public class editingBoard extends JFrame {
         team2 = controller.getTeam2();
         grass.setSize(1366,768);
         info =new JLabel("");
-        //info.setText("<html> Current Champion: <br>" +  + "Current HP</html>");
         info.setBounds(5,20,170,130);
         info.setVerticalAlignment(JLabel.TOP);
         info.setOpaque(true);
         info.setBackground(Color.white);
 
-        abilitiesPanel = new JPanel();
-        abilitiesPanel.setBounds(30, 180,225,500);
-        abilitiesPanel.setBackground(Color.white);
-        abilitiesPanel.setOpaque(true);
-
-        effectsPanel = new JPanel();
-        effectsPanel.setBounds(1100,284,225,220);
-        effectsPanel.setBackground(Color.WHITE);
-        effectsPanel.setOpaque(true);
-
-        JLabel abilitiesLabel = new JLabel("<html><p style=\"font-size:14pt\">Abilities: </p></html>");
-        JLabel effectsLabel = new JLabel("<html><p style=\"font-size:14pt\">Applied Effects: </p></html>");
-
-        abilitiesInfo = new JTextArea();
-        abilitiesInfo.setEditable(false);
-
-        effectsInfo = new JTextArea();
-        effectsInfo.setEditable(false);
-
-        abilitiesPanel.add(abilitiesLabel);
-        abilitiesPanel.add(abilitiesInfo);
-
-        effectsPanel.add(effectsLabel);
-        effectsPanel.add(effectsInfo);
-
-        //abilitiesAndEffects.add(effectsInfo);
-
-        queuePanel = new JPanel(new GridLayout(1,2));
-        queuePanel.setBounds(185,20,145,130);
-        queuePanel.setBackground(Color.WHITE);
-        queuePanel.setOpaque(true);
-
-        JLabel queueLabel = new JLabel("Turn Order:");
-        queueLabel.setVerticalAlignment(JLabel.TOP);
-
-        queue = new JTextArea();
-        queue.setEditable(false);
-        queue.setBackground(Color.WHITE);
-        queue.setOpaque(true);
-
-        queuePanel.add(queueLabel);
-        queuePanel.add(queue);
-
         board = controller.getCurrentGame().getBoard();
-
         drawBoard(board);
+        PlaceIcons();
+        PlacePlayersLabels();
+        PlaceAbilitiesAndEffects();
+        PlaceQueue();
+        PlaceEndTurnButton();
+
+        displayQueue(queue, info);
+
+        panel.add(panel2,Integer.valueOf(1));
+        panel.add(player1Label,Integer.valueOf(2));
+        panel.add(player2Label,Integer.valueOf(2));
+        panel.add(player1Panel,Integer.valueOf(1));
+        panel.add(player2panel,Integer.valueOf(1));
+        panel.add(leader1Ability,Integer.valueOf(1));
+        panel.add(ability1Used,Integer.valueOf(2));
+        panel.add(leader2Ability,Integer.valueOf(1));
+        panel.add(ability2Used,Integer.valueOf(2));
+        panel.add(info,Integer.valueOf(1));
+        panel.add(abilitiesPanel, Integer.valueOf(1));
+        panel.add(effectsPanel,Integer.valueOf(1));
+        panel.add(queuePanel, Integer.valueOf(1));
+        panel.add(grass,Integer.valueOf(0));
+        panel.add(endTurnPanel,Integer.valueOf(1));
+
 
         this.setDefaultCloseOperation(this.EXIT_ON_CLOSE);
         this.getContentPane().add(panel);
@@ -102,7 +85,7 @@ public class editingBoard extends JFrame {
     }
 
     public void drawBoard(Object[][] board) {
-        JLayeredPane panel2 = new JLayeredPane();
+        panel2 = new JLayeredPane();
         panel2.setLayout(new GridLayout(board.length, board[0].length, 5, 5));
         panel2.setBounds(333, 16, 700, 700);
 
@@ -156,146 +139,19 @@ public class editingBoard extends JFrame {
 
                     @Override
                     public void mouseMoved(MouseEvent e) {
-                        //if (i==100) {
-                        //info.setText("<html> Current Champion <br> Current HP</html>");
-                        //}
-                        //else {queue.setText("Leader "+ champion.getLocation()+"\n"+"Current HP");
-                        //queue.setBounds(120,i+100,300,50);//}
-                        //frame.getContentPane().add(panel2);
-                        displayQueue(queue,info);
+                        //displayQueue(queue,info);
                         frame.revalidate();
-                        //if(leader.getText()!="" && queue.getText()!="") forward.setEnabled(true);
                     }
                 }
         );
 
-        final int ICON_WIDTH = 75;
-        final int ROWS = 2;
 
-        // for (int i = 0; i < ROWS; i++) {
-        // 	// Fetch team "i"
-
-        // 	for (int j = 0; /*j < team[i].size*/; j++) {
-        // 		// Fetch champion j from team
-        // 		// Champion curr = team.get(j);
-
-        // 		// Fetch Icon of champion
-        // 		// ImageIcon iconChamp = new ImageIcon("assets/characters/64/" + curr.getName() + ".png");
-        // 	}
-        // }
-
-
-        // placing each player's champions for display
-        int j =1100;
-        for (int ind = 0; ind < 6; ind++) {
-            j = (ind==3? 1100 : j);
-            Champion realChampion;
-
-            if(ind <= 2){
-                ImageIcon btn = new ImageIcon("assets/characters/64/" + team1.get(ind).getName() + ".png");
-                champion = new JLabel(btn);
-                realChampion = team1.get(ind);
-            }
-            else{
-                ImageIcon btn = new ImageIcon("assets/characters/64/" + team2.get(ind-3).getName() + ".png");
-                champion = new JLabel(btn);
-                realChampion = team2.get(ind-3);
-            }
-
-            abilities = realChampion.getAbilities();
-            appliedEffects = realChampion.getAppliedEffects();
-
-            int i = (ind<3? 100 : 200);
-            champion.setBounds(j,i,64,64);
-            champion.addMouseMotionListener(new MouseMotionListener() {
-                @Override
-                public void mouseDragged(MouseEvent e) {}
-
-                @Override
-                public void mouseMoved(MouseEvent e) {
-                    //champion = (JLabel) e.getSource();
-                    //if (i==100) {
-                    String type = getType(realChampion.getClass().getName());
-                    info.setText("<html>" + realChampion.getName() + "<br>Type: " + type + "<br>Current HP: "
-                            + realChampion.getCurrentHP() + "<br>Mana: " + realChampion.getMana() + "<br>Action Points: " +
-                            realChampion.getCurrentActionPoints() + "<br>Attack Damage: " + realChampion.getAttackDamage() +
-                            "<br>Attack Range: " + realChampion.getAttackRange() + "<br>Leader? " + isLeader(realChampion) + "</html>");
-                    abilitiesInfo.setText("");
-                    effectsInfo.setText("");
-                    displayAbilitiesAndEffects(realChampion,abilitiesInfo,effectsInfo);
-
-
-                    //}
-                    //else {queue.setText("Leader "+ champion.getLocation()+"\n"+"Current HP");
-                    //queue.setBounds(120,i+100,300,50);//}
-                    //frame.getContentPane().add(panel2);
-                    frame.revalidate();
-                    frame.repaint();
-                    //if(leader.getText()!="" && queue.getText()!="") forward.setEnabled(true);
-                }
-            });
-            panel.add(champion,Integer.valueOf(0));
-            j+=75;
-        }
-
-        JPanel p1 = new JPanel();
-        p1.setBounds(1080, 60, 90, 35);
-        p1.setBackground(Color.white);
-
-        JPanel leader1Ability = new JPanel();
-        leader1Ability.setBounds(1180,60,150,35);
-        leader1Ability.setBackground(Color.WHITE);
-
-        JLabel ability1Used = new JLabel();
-        if(controller.getCurrentGame().isFirstLeaderAbilityUsed()) ability1Used.setText("Leader's ability used");
-        else ability1Used.setText("Leader's ability not used");
-        ability1Used.setBounds(1180,60,150,35);
-        ability1Used.setHorizontalAlignment(JLabel.CENTER);
-
-        player1 =  new JLabel(controller.getPlayer1().getName() + "'s team");
-        player1.setBounds(1080, 60, 100, 30);
-        player1.setHorizontalAlignment(JLabel.LEFT);
-
-        JPanel p2 = new JPanel();
-        p2.setBounds(1080, 160, 90, 35);
-        p2.setBackground(Color.white);
-
-        JPanel leader2Ability = new JPanel();
-        leader2Ability.setBounds(1180,160,150,35);
-        leader2Ability.setBackground(Color.WHITE);
-
-        JLabel ability2Used = new JLabel();
-        if(controller.getCurrentGame().isSecondLeaderAbilityUsed()) ability2Used.setText("Leader's ability used");
-        else ability2Used.setText("Leader's ability not used");
-        ability2Used.setBounds(1180,160,150,35);
-        ability2Used.setHorizontalAlignment(JLabel.CENTER);
-
-        player2 =  new JLabel(controller.getPlayer2().getName() + "'s team");
-        player2.setBounds(1080, 160, 100, 30);
-        player2.setForeground(Color.black);
-        player2.setHorizontalAlignment(JLabel.LEFT);
-
-        displayQueue(queue, info);
-        panel.add(panel2,Integer.valueOf(1));
-        panel.add(player1,Integer.valueOf(2));
-        panel.add(player2,Integer.valueOf(2));
-        panel.add(p1,Integer.valueOf(1));
-        panel.add(p2,Integer.valueOf(1));
-        panel.add(leader1Ability,Integer.valueOf(1));
-        panel.add(ability1Used,Integer.valueOf(2));
-        panel.add(leader2Ability,Integer.valueOf(1));
-        panel.add(ability2Used,Integer.valueOf(2));
-        panel.add(info,Integer.valueOf(1));
-        panel.add(abilitiesPanel, Integer.valueOf(1));
-        panel.add(effectsPanel,Integer.valueOf(1));
-        panel.add(queuePanel, Integer.valueOf(1));
-        panel.add(grass,Integer.valueOf(0));
 
     }
 
-    public static void main(String[] args) {
-        BoardView view = new BoardView();
-    }
+//    public static void main(String[] args) {
+//        BoardView view = new BoardView();
+//    }
 
     public class baseBackground extends JPanel{
         public void paintComponent(Graphics g) {
@@ -321,7 +177,7 @@ public class editingBoard extends JFrame {
         String type = getType(c.getClass().getName());
 
         info.setText("<html> Current Champion: " + c.getName() + "<br>Type: " + type + "<br>Current HP: " +
-                c.getCurrentHP() + "<br>Mana: " + c.getMana() + "<br>Action Points: " + c.getCurrentActionPoints() +
+                c.getCurrentHP() + "/" + c.getMaxHP() + "<br>Mana: " + c.getMana() + "<br>Action Points: " + c.getCurrentActionPoints() +
                 "<br>Attack Damage: " + c.getAttackDamage() + "<br>Attack Range: " + c.getAttackRange() + "<br>Leader? " +
                 isLeader(c) + "</html>");
         abilitiesInfo.setText("");
@@ -391,6 +247,162 @@ public class editingBoard extends JFrame {
     private String isLeader(Champion c){
         if(c == controller.getPlayer1().getLeader() || c == controller.getPlayer2().getLeader()) return "Yes";
         else return "No";
+    }
+
+    private void PlaceIcons(){
+        int j =1100;
+        for (int ind = 0; ind < 6; ind++) {
+            j = (ind==3? 1100 : j);
+            Champion realChampion;
+
+            if(ind <= 2){
+                ImageIcon btn = new ImageIcon("assets/characters/64/" + team1.get(ind).getName() + ".png");
+                champion = new JLabel(btn);
+                realChampion = team1.get(ind);
+            }
+            else{
+                ImageIcon btn = new ImageIcon("assets/characters/64/" + team2.get(ind-3).getName() + ".png");
+                champion = new JLabel(btn);
+                realChampion = team2.get(ind-3);
+            }
+
+            abilities = realChampion.getAbilities();
+            appliedEffects = realChampion.getAppliedEffects();
+
+            int i = (ind<3? 100 : 200);
+            champion.setBounds(j,i,64,64);
+            champion.addMouseMotionListener(new MouseMotionListener() {
+                @Override
+                public void mouseDragged(MouseEvent e) {}
+
+                @Override
+                public void mouseMoved(MouseEvent e) {
+                    String type = getType(realChampion.getClass().getName());
+                    info.setText("<html>" + realChampion.getName() + "<br>Type: " + type + "<br>Current HP: "
+                            + realChampion.getCurrentHP() + "/" + realChampion.getMaxHP() + "<br>Mana: " + realChampion.getMana() + "<br>Action Points: " +
+                            realChampion.getCurrentActionPoints() + "<br>Attack Damage: " + realChampion.getAttackDamage() +
+                            "<br>Attack Range: " + realChampion.getAttackRange() + "<br>Leader? " + isLeader(realChampion) + "</html>");
+                    abilitiesInfo.setText("");
+                    effectsInfo.setText("");
+                    displayAbilitiesAndEffects(realChampion,abilitiesInfo,effectsInfo);
+                    frame.revalidate();
+                    frame.repaint();
+
+                }
+            });
+            panel.add(champion,Integer.valueOf(0));
+            j+=75;
+        }
+
+    }
+
+    private void PlaceAbilitiesAndEffects(){
+        abilitiesPanel = new JPanel();
+        abilitiesPanel.setBounds(30, 180,225,500);
+        abilitiesPanel.setBackground(Color.white);
+        abilitiesPanel.setOpaque(true);
+
+        JLabel abilitiesLabel = new JLabel("<html><p style=\"font-size:14pt\">Abilities: </p></html>");
+
+        abilitiesInfo = new JTextArea();
+        abilitiesInfo.setEditable(false);
+
+        abilitiesPanel.add(abilitiesLabel);
+        abilitiesPanel.add(abilitiesInfo);
+
+
+        effectsPanel = new JPanel();
+        effectsPanel.setBounds(1100,284,225,220);
+        effectsPanel.setBackground(Color.WHITE);
+        effectsPanel.setOpaque(true);
+
+        JLabel effectsLabel = new JLabel("<html><p style=\"font-size:14pt\">Applied Effects: </p></html>");
+
+        effectsInfo = new JTextArea();
+        effectsInfo.setEditable(false);
+
+        effectsPanel.add(effectsLabel);
+        effectsPanel.add(effectsInfo);
+    }
+
+    private void PlaceQueue(){
+        queuePanel = new JPanel(new GridLayout(1,2));
+        queuePanel.setBounds(185,20,145,130);
+        queuePanel.setBackground(Color.WHITE);
+        queuePanel.setOpaque(true);
+
+        JLabel queueLabel = new JLabel("Turn Order:");
+        queueLabel.setVerticalAlignment(JLabel.TOP);
+
+        queue = new JTextArea();
+        queue.setEditable(false);
+        queue.setBackground(Color.WHITE);
+        queue.setOpaque(true);
+
+        queuePanel.add(queueLabel);
+        queuePanel.add(queue);
+    }
+
+    private void PlacePlayersLabels(){
+        player1Panel = new JPanel();
+        player1Panel.setBounds(1080, 60, 90, 35);
+        player1Panel.setBackground(Color.white);
+
+        player1Label =  new JLabel(controller.getPlayer1().getName() + "'s team");
+        player1Label.setBounds(1080, 60, 100, 30);
+        player1Label.setHorizontalAlignment(JLabel.LEFT);
+
+
+        leader1Ability = new JPanel();
+        leader1Ability.setBounds(1180,60,150,35);
+        leader1Ability.setBackground(Color.WHITE);
+
+        ability1Used = new JLabel();
+        if(controller.getCurrentGame().isFirstLeaderAbilityUsed()) ability1Used.setText("Leader's ability used");
+        else ability1Used.setText("Leader's ability not used");
+        ability1Used.setBounds(1180,60,150,35);
+        ability1Used.setHorizontalAlignment(JLabel.CENTER);
+
+
+        player2panel = new JPanel();
+        player2panel.setBounds(1080, 160, 90, 35);
+        player2panel.setBackground(Color.white);
+
+        player2Label =  new JLabel(controller.getPlayer2().getName() + "'s team");
+        player2Label.setBounds(1080, 160, 100, 30);
+        player2Label.setForeground(Color.black);
+        player2Label.setHorizontalAlignment(JLabel.LEFT);
+
+
+        leader2Ability = new JPanel();
+        leader2Ability.setBounds(1180,160,150,35);
+        leader2Ability.setBackground(Color.WHITE);
+
+        ability2Used = new JLabel();
+        if(controller.getCurrentGame().isSecondLeaderAbilityUsed()) ability2Used.setText("Leader's ability used");
+        else ability2Used.setText("Leader's ability not used");
+        ability2Used.setBounds(1180,160,150,35);
+        ability2Used.setHorizontalAlignment(JLabel.CENTER);
+
+    }
+
+    private void PlaceEndTurnButton(){
+        endTurnPanel = new JPanel();
+        endTurnPanel.setBounds(1100,600,80,30);
+        endTurnPanel.setBackground(Color.WHITE);
+        endTurnPanel.setOpaque(true);
+
+        endTurn = new JButton("End Turn");
+        endTurn.setVisible(true);
+        endTurn.setForeground(Color.black);
+        endTurn.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                drawBoard(board);
+            }
+        });
+
+        endTurnPanel.add(endTurn);
     }
 }
 
